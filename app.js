5036d32862ac0e32a527dc3a23d30b0b842aa656
Sandeep Padhi <padhisandeep96@gmail.com>
2018-11-07 23:59:27 +0530

    My first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+var express = require("express");
+var app = new express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var path=require("path");
+
+var port = process.env.port || 3000;
+
+app.use(express.static(__dirname + "/public" ));
+
+app.get('/',function(req,res){
+    var pth=path.join(__dirname,"/public/index.html");
+    res.sendFile(pth);
+    //res.redirect("index.html");
+});
+
+app.get("/emit",function(req,res){
+    console.log("emit");
+var pth=path.join(__dirname,"/public/emit.html");
+    res.sendFile(pth);
+
+
+});
+
+
+app.get("/visualize",function(req,res){
+    console.log("visualize");
+    var pth=path.join(__dirname,"/public/visualize.html");
+        res.sendFile(pth);
+    
+    
+    });
+    
+
+var framecount=0;
+
+io.on('connection',function(socket){
+   console.log("user connected..!!");
+
+   socket.on('join',function(data){
+        socket.join(data.email);
+        console.log("ENTERED:"+data.email);
+        //var clients = io.sockets.clients();
+        //console.log(clients);
+
+        //io.sockets.in("sandeep").emit("stream","sandeep is connected...");
+    });
+
+
+    socket.on('stream',function(image){
+        console.log("Streaming video image..!!!");
+        
+        //socket.broadcast.emit('stream',image).then(console.log("video send..!!1"));  
+        //io.emit('stream',image);
+
+   framecount=framecount+1;
+   if(framecount%5==0){
+        
+        io.sockets.in(image.email).emit("stream",image);//here "sandeep"  represents room name..!!!
+   }
+
+        console.log("video send..!!!");
+        
+    });
+
+});
+
+
+/*var ip="192.168.0.106";
+
+
+http.listen(port,ip,function(){
+console.log("Server running at port "+ port);
+});
+
+*/
+
+
+http.listen(port,function(){
+    console.log("Server running at port "+ port);
+    });
