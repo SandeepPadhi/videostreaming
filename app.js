76891cafcb3524c3aade6a6ebda7a5763b9e4422
Sandeep Padhi <padhisandeep96@gmail.com>
2018-11-08 10:53:12 +0530

    copied from index.js and commented previous code

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/*
+
+
 var express = require("express");
 var app = new express();
 var http = require("http").Server(app);
@@ -78,6 +81,7 @@ console.log("Server running at port "+ port);
 });
 
 */
+/*
 const hostname = '0.0.0.0';
 
 
@@ -85,3 +89,36 @@ http.listen(port,hostname,function(){
     console.log("Server running at.. port "+ port);
     
     });
+
+
+
+ */   
+
+
+
+
+const http = require('http')
+const url = require('url')
+
+const service = require('./service')
+
+const hostname = '0.0.0.0';
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+
+  if (req.method === 'GET') {
+
+    const path = url.parse(req.url).pathname.slice(1).replace(/%20/g, ' ')
+    
+    res.writeHead(200, {'Content-Type': 'application/json'})    
+    //res.write(JSON.stringify(service(path)));
+    res.write("Hello heroku from Sandeep..!!!");
+    res.write("Happy Diwali");
+    res.end();
+
+  }
+
+}).listen(port, hostname, function(){
+  console.log('Server running at http://${' + hostname + '}:${' + port + '}/');
+})
